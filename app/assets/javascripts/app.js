93b5ddc97e256893d6dfbba7eb458881068fc7af
Alex Afeltra <afeltra.alex@gmail.com>
2017-03-06 17:06:01 -0500

    angular back

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,52 +1,51 @@
-// var app = angular.module('webApp', ['ngRoute', 'templates']);
+var app = angular.module('webApp', ['ngRoute', 'templates']);
 
-// app.config([
-//            "$routeProvider",
-//   function($routeProvider) {
-//     $routeProvider.when("/", {
-//        controller: "HomeController",
-//       templateUrl: "home.html"
-//     }).when("/portfolio", {
-//     	controller: "PortfolioController",
-//     	templateUrl: "portfolio.html"
-//     }).when("/contact", {
-//       controller: "ContactController",
-//       templateUrl: "contact.html"
-//     });
+app.config([
+           "$routeProvider",
+  function($routeProvider) {
+    $routeProvider.when("/", {
+       controller: "HomeController",
+      templateUrl: "home.html"
+    }).when("/portfolio", {
+    	controller: "PortfolioController",
+    	templateUrl: "portfolio.html"
+    }).when("/contact", {
+      controller: "ContactController",
+      templateUrl: "contact.html"
+    });
     
-//   }
-// ]);
+  }
+]);
 
-// app.controller("HomeController",[
-//           "$scope","$http","$location",
-//   function($scope, $http, $location){
-//   	$http.get("/");
-//   	$scope.updates = ['<br><br><br>Hi, my name is Alex.<br>Discover who I am'];
-//         $(function(){
-//             $(".update-box p").typed({
-//               strings:$scope.updates,
-//               typeSpeed: 50,
-//               loop: false,
-//              // backSpeed: 50,
-//              // backDelay: 300,
-//               contentType: 'html',
-//               loopCount: false,
-//               showCursor: false,
-//               cursorChar: "|"
-//             });
-//           }); 
-//   	}
-//  ]);
+app.controller("HomeController",[
+          "$scope","$http","$location",
+  function($scope, $http, $location){
+  	$http.get("/");
+  	$scope.updates = ['<br><br><br>Hi, my name is Alex.<br>Discover who I am'];
+        $(function(){
+            $(".update-box p").typed({
+              strings:$scope.updates,
+              typeSpeed: 50,
+              loop: false,
+             // backSpeed: 50,
+             // backDelay: 300,
+              contentType: 'html',
+              loopCount: false,
+              showCursor: false,
+              cursorChar: "|"
+            });
+          }); 
+  	}
+ ]);
 
-// app.controller("PortfolioController",[
-//           "$scope","$http","$location",
-//   function($scope, $http, $location){
-//   }
-//  ]);
+app.controller("PortfolioController",[
+          "$scope","$http","$location",
+  function($scope, $http, $location){
+  }
+ ]);
 
-// app.controller("ContactController",[
-//           "$scope","$http","$location",
-//   function($scope, $http, $location){
-//   }
-//  ]);
-
+app.controller("ContactController",[
+          "$scope","$http","$location",
+  function($scope, $http, $location){
+  }
+ ]);
