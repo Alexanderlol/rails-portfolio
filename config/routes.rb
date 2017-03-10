c0a099619bf809a953638598060567b3fc63c9cc
Alex Afeltra <afeltra.alex@gmail.com>
2017-03-10 17:29:01 -0500

    file structure for blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: 'home#index'
-	get "portfolio", to: "portfolio#index" 
 	resources :contacts, only: :create
 	get 'contact', to: 'contacts#new', as: 'new_contact'
 end
